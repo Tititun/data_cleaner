02328c9c7a4ac9fd1e3bb6824e6679d674d8989d
danil <vinniopo@mail.ru>
2024-05-30 01:55:46 +0700

    dropdown value saved

--- data/static/data/js/components/dropdown.js
+++ data/static/data/js/components/dropdown.js
@@ -1,31 +1,31 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
 
-export const Dropdown = function ({currentName, nameList, removeDropbar}) {
+export const Dropdown = function ({currentName, nameList, removeDropbar, level}) {
+    console.log('rendering dropdown')
     const ref = React.useRef(null)
+    const [filter, setFilter] = React.useState('')
 
-    function closeDropdown () {
+    function closeDropdown (value) {
         ref.current &&  ref.current.classList.remove('is-active')
-        removeDropbar()
+        removeDropbar(value, level)
     }
-    const timer = setTimeout(removeDropbar, 3000);
 
     return (
-        <div ref={ref} className="dropdown is-hoverable is-active">
-            <div className="dropdown-trigger">
-                <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
-                <span>{currentName ? currentName : ''}</span>
-                <span className="icon is-small">
-                    <i className="fas fa-angle-down" aria-hidden="true"></i>
-                </span>
-                </button>
-            </div>
-            <div className="dropdown-menu" role="menu" onMouseLeave={closeDropdown} onMouseEnter={() => timer && clearInterval(timer)}>
+        <div ref={ref} className="dropdown is-hoverable is-active" onMouseLeave={() => closeDropdown(currentName)}>
+            <input className="input" type="text" onChange={e => setFilter(e.target.value)} onKeyUp={ e => {
+                                                                                            if (e.key === 'Enter') { closeDropdown(e.target.value) }
+                                                                                        }}> 
+                
+            </input>
+            <div className="dropdown-menu" role="menu" >
                 <div cclassNamelass="dropdown-content">
                     {
-                        nameList.map((name, idx) => {
+                        nameList.filter(name => name.search(filter) !== -1).map((name, idx) => {
                             return (
-                            <div key={idx} class={`dropdown-item has-background-${name === 'add category' ? 'info-light' : 'white'}`} onClick={closeDropdown}>
+                            <div key={idx}
+                             class={`dropdown-item has-background-${name === 'add category' ? 'info-light' : 'white'}`}
+                             onClick={() => closeDropdown(name)}>
                                 <p>{name}</p>
                             </div>
                             )
--- data/static/data/js/components/item.js
+++ data/static/data/js/components/item.js
@@ -5,30 +5,40 @@ import { Dropdown } from './dropdown';
 export default React.memo(function ({item, levels}) {
   console.log('rendering item')
   const [dropbar, setDropbar] = React.useState({})
+  const [item_levels, setItemLevels] = React.useState({
+    level_1: item['level_1'] || item['level_1_inferred'] || '',
+    level_2: item['level_2'] || item['level_2_inferred'] || '',
+    level_3: item['level_3'] || item['level_3_inferred'] || ''
+  })
 
-  const level_1 = item['level_1'] || item['level_1_inferred'] || ''
-  const level_2 = item['level_2'] || item['level_2_inferred'] || ''
-  const level_3 = item['level_3'] || item['level_3_inferred'] || ''
 
   function levelClicked(e) {
     const level = e.target.dataset.level;
     if (!level) {return}
-    const names = ['add category']
+    const names = []
 
     if (level === '1') {
-      names.push(...(levels?.[level_2]?.l1 || []))
-      names.push(...(levels?.[level_3]?.l1 || []))
+      names.push(...(levels?.[item_levels.level_2]?.l1 || []))
+      names.push(...(levels?.[item_levels.level_3]?.l1 || []))
     } else if (level === '2') {
-      names.push(...(levels?.[level_1]?.l2 || []))
-      names.push(...(levels?.[level_3]?.l2 || []))
+      names.push(...(levels?.[item_levels.level_1]?.l2 || []))
+      names.push(...(levels?.[item_levels.level_3]?.l2 || []))
     } else if (level === '3') {
-      // names.push(...(levels?.[level_1]?.l3 || []))
-      names.push(...(levels?.[level_2]?.l3 || []))
+      names.push(...(levels?.[item_levels.level_1]?.l3 || []))
+      names.push(...(levels?.[item_levels.level_2]?.l3 || []))
     }
-    console.log(names)
+    names.sort()
     setDropbar({...dropbar, [level]: names})
   }
-  console.log(dropbar)
+
+  function closeDropdown(value, level) {
+    if (value){
+      item[level] = value;
+      setItemLevels(structuredClone({...item_levels, [level]: value}))
+    }
+    setDropbar(null)
+  }
+
   return (
       <tr key={item.id}>
         <td className='w-5' key={1}>{item.source}</td>
@@ -36,16 +46,16 @@ export default React.memo(function ({item, levels}) {
         <td className='w-20'  key={3}>{item.name}</td>
         <td className='w-5'  key={4}></td>
         <td data-level={1} className='w-15' key={5} onClick={levelClicked}>
-          {dropbar?.['1'] ? <Dropdown currentName={level_1} nameList={dropbar['1']}
-           removeDropbar={() => setDropbar(null)}/>: level_1}
+          {dropbar?.['1'] ? <Dropdown currentName={item_levels.level_1} nameList={dropbar['1']} level="level_1"
+           removeDropbar={closeDropdown}/>: item_levels.level_1}
         </td>
         <td data-level={2} className='w-15 has-background-light has-text-centered' key={6} onClick={levelClicked}>
-          {dropbar?.['2'] ? <Dropdown currentName={level_2} nameList={dropbar['2']}
-           removeDropbar={() => setDropbar(null)}/>: level_2}
+          {dropbar?.['2'] ? <Dropdown currentName={item_levels.level_2} nameList={dropbar['2']} level="level_2"
+           removeDropbar={closeDropdown}/>: item_levels.level_2}
         </td>
         <td data-level={3} className='w-15' key={7} onClick={levelClicked}>
-         {dropbar?.['3'] ? <Dropdown currentName={level_3} nameList={dropbar['3']}
-           removeDropbar={() => setDropbar(null)}/>: level_3}
+         {dropbar?.['3'] ? <Dropdown currentName={item_levels.level_3} nameList={dropbar['3']} level="level_3"
+           removeDropbar={closeDropdown}/>: item_levels.level_3}
         </td>
         <td className='w-5'  key={8}>
           <button className='button is-light m-0'>Save</button>
